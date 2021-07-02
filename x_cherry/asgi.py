864d8580947dcb0eadad0c52da1257fad46daa7d
andrius-siup <andrius.siupinys@gmail.com>
2021-07-02 11:06:48 +0000

    Initial commit

---
+++ x_cherry/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for x_cherry project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'x_cherry.settings')
+
+application = get_asgi_application()
