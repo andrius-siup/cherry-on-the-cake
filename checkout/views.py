64ee9b3cc4ecafd709686c60e357b29cdaea63d5
andrius-siup <andrius.siupinys@gmail.com>
2021-07-12 13:33:25 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51J3fnNDq6Ek3HX07Qsxg6cT9g8vIDag5HE6mgCtYkH9KJ2RpzgKcXTXhSCWA6UPbmvKbKnbf1NGDkGtiUgpa4JX300Qr3IPmuH',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
