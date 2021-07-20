1635c41a701e1df801879be0cddfc58da79c9f9b
andrius-siup <andrius.siupinys@gmail.com>
2021-07-20 13:00:14 +0000

    Added security access to the add product page

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
+from django.contrib.admin.views.decorators import staff_member_required
 
 from .models import Product, Category
 from .forms import ProductForm
@@ -73,6 +74,7 @@ def product_detail(request, product_id):
     return render(request, 'products/product_detail.html', context)
 
 
+@staff_member_required
 def add_product(request):
     """Add a product to the store"""
     form = ProductForm()
