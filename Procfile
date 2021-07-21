9403c439db924ef579f772d3fc4d2268bb23ff7d
andrius-siup <andrius.siupinys@gmail.com>
2021-07-21 17:14:13 +0000

    Deployment part-1

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn x_cherry.wsgi:application
--- x_cherry/settings.py
+++ x_cherry/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['andrius-siup-x-cherry.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -122,12 +122,17 @@ WSGI_APPLICATION = 'x_cherry.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABSES = {
+        'default': dj_database_url.parse(os.environ.get('DATABSE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
