d17399acc47041d3a103dbaeaead77721773a243
andrius-siup <andrius.siupinys@gmail.com>
2021-07-28 14:39:06 +0000

    Added Blog post admin section

--- blog/admin.py
+++ blog/admin.py
@@ -2,22 +2,25 @@ from django.contrib import admin
 from .models import Post, Category
 
 
-# class PostAdmin(admin.ModelAdmin):
-#     list_display = (
-#         'title',
-#         'content',
-#         'created_date',
-#         'created_by',
-#     )
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'content',
+        'created_by',
+        'created_date',
+    )
 
-#     ordering = ('created_date')
-
-
-# class CategoryAdmin(admin.ModelAdmin):
-#     list_display = (
-#         'name'
-#     )
+    ordering = ('created_date',)
 
 
-admin.site.register(Post)  # PostAdmin
-admin.site.register(Category)  # CategoryAdmin
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+    ordering = ('name',)
+
+
+admin.site.register(Post, PostAdmin)  # PostAdmin
+admin.site.register(Category, CategoryAdmin)  # CategoryAdmin
