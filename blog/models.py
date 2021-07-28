5dacd02ba5fff5306700fa13df97bec5b47859be
andrius-siup <andrius.siupinys@gmail.com>
2021-07-28 13:10:13 +0000

    Created Blog post model and run migrations

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=254, null=True, blank=True)
+    content = models.CharField(max_length=100000, null=True, blank=True)
+    created_date = models.DateTimeField(auto_now=True)
+    created_by = models.CharField(max_length=25, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
