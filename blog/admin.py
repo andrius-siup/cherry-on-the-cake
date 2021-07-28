173db53a1cb2bf99c96c4a82b3b093cf3609b1da
andrius-siup <andrius.siupinys@gmail.com>
2021-07-28 14:16:35 +0000

    Changed the Blog post model, add categories field

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Category
 
 
-admin.site.register(Post)
+# class PostAdmin(admin.ModelAdmin):
+#     list_display = (
+#         'title',
+#         'content',
+#         'created_date',
+#         'created_by',
+#     )
+
+#     ordering = ('created_date')
+
+
+# class CategoryAdmin(admin.ModelAdmin):
+#     list_display = (
+#         'name'
+#     )
+
+
+admin.site.register(Post)  # PostAdmin
+admin.site.register(Category)  # CategoryAdmin
--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,23 @@ from django.db import models
 from django.utils import timezone
 
 
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
 class Post(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=254, null=True, blank=True)
     content = models.CharField(max_length=100000, null=True, blank=True)
     created_date = models.DateTimeField(auto_now=True)
