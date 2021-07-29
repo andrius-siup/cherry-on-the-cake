6ecbde42a2d51cf70ea01529d275c2dfa0414dbd
andrius-siup <andrius.siupinys@gmail.com>
2021-07-29 15:51:04 +0000

    Created view to access into Blog post categories, showing all finds posts with same category

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Post
+from .models import Post, Category
 
 
 def posts(request):
@@ -9,8 +9,14 @@ def posts(request):
 
     posts = Post.objects.all()
     query = None
+    categories = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            posts = posts.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -23,6 +29,7 @@ def posts(request):
     context = {
         'posts': posts,
         'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, "blog/blogs.html", context)
