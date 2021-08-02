10accdffdc212b9066cdf5b348f8480b82e3b4a6
andrius-siup <andrius.siupinys@gmail.com>
2021-08-02 14:15:53 +0000

    Added url for post comments

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('add/', views.add_blog_post, name='add_blog_post'),
     path('edit/<int:post_id>/', views.edit_blog_post, name='edit_blog_post'),
     path('delete/<int:post_id>/', views.delete_blog_post, name='delete_blog_post'),
+    path('add_comment/<int:post_id>/', views.add_post_comment, name='add_post_comment'),
 ]
