3a97885eb900e75db1a5bf7e773600c4bec18851
andrius-siup <andrius.siupinys@gmail.com>
2021-08-02 14:14:59 +0000

    Created class for comment form and display all field from comment post model

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Post, Category
+from .models import Post, Category, BlogPostComment
 
 
 class PostForm(forms.ModelForm):
@@ -14,5 +14,18 @@ class PostForm(forms.ModelForm):
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
         self.fields['category'].choises = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
+
+
+class BlogPostCommentForm(forms.ModelForm):
+
+    class Meta:
+        model = BlogPostComment
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
