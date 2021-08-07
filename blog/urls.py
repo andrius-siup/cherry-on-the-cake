6e6a12b035222af79d0b79a172336258146bdd20
andrius-siup <andrius.siupinys@gmail.com>
2021-08-07 14:39:39 +0000

    Added delete post comment url and link to that

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('edit/<int:post_id>/', views.edit_blog_post, name='edit_blog_post'),
     path('delete/<int:post_id>/', views.delete_blog_post, name='delete_blog_post'),
     path('add_comment/<int:post_id>/', views.add_post_comment, name='add_post_comment'),
+    path('delete_comment/<int:comment_id>/', views.delete_post_comment, name='delete_post_comment'),
 ]
