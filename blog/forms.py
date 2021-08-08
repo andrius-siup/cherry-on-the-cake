a9558e804c2d35ee3a8fee501f3e867c0665b45d
andrius-siup <andrius.siupinys@gmail.com>
2021-08-08 09:08:49 +0000

    Removed add post form labels, and added class

--- blog/forms.py
+++ blog/forms.py
@@ -10,12 +10,29 @@ class PostForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        placeholders = {
+            'title': 'Post Title',
+            'category': 'Category',
+            'content': 'Add text here',
+            'created_by': 'Created by',
+        }
+
+        self.fields['title'].widget.attrs['autofocus'] = True  # cursore start here
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
+
         categories = Category.objects.all()
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
         self.fields['category'].choises = friendly_names
-        for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'border-black rounded-0'
+        # for field_name, field in self.fields.items():
+        #     field.widget.attrs['class'] = 'border-black rounded-0'
 
 
 class BlogPostCommentForm(forms.ModelForm):
