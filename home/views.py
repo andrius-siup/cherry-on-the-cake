3723f93a98695bfe48a8f6c2df14c76e5804b336
andrius-siup <andrius.siupinys@gmail.com>
2021-08-13 15:34:15 +0000

    Created 404 Error page

--- home/views.py
+++ home/views.py
@@ -7,3 +7,7 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html', {'page_title': 'Home'})
+
+
+def error_404_view(request, exception):
+    return render(request, '404.html')
--- x_cherry/settings.py
+++ x_cherry/settings.py
@@ -28,6 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production! Before submit False
 DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = False
 
 ALLOWED_HOSTS = ['andrius-siup-x-cherry.herokuapp.com', 'localhost']
 
