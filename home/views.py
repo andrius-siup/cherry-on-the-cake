f5b25639c565095b1676266200068c277be77929
andrius-siup <andrius.siupinys@gmail.com>
2021-08-26 09:08:56 +0000

    Created 500 error template and checking

--- home/views.py
+++ home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here.from django.shortcuts import render_to_response
 
 
 def index(request):
@@ -11,3 +11,7 @@ def index(request):
 
 def error_404_view(request, exception):
     return render(request, '404.html')
+
+
+def error_500_view(request):
+    return render(request, '500.html')
